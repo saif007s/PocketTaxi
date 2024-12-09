37386bad32a6eb8259287406bc86312614eede15
saif007s <saifahmad786sm@gmail.com>
2024-12-09 18:07:30 +0530

    first

---
+++ app/(root)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from "expo-router";
+import React from "react";
+
+const Layout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="find-ride" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="confirm-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="book-ride"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
+};
+
+export default Layout;
